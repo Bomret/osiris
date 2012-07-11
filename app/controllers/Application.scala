d3496c53514bd8e1b7ec95f3a3c7ec44d0f0de6a
Stefan Reichel <dev@bomret.com>
2012-07-11 21:43:42 +0200

    New controller that returns the currently available shaders as json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,8 +1,10 @@
 package controllers
 
-import play.api._
-import libs.iteratee._
+import play.api.libs.iteratee._
 import play.api.mvc._
+import io.Source
+import play.api.libs.json.Json
+import javax.accessibility.AccessibleTable
 
 object Application extends Controller {
 
@@ -10,10 +12,28 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def getShaders = Action {
+    val availableShaders = Source.fromFile("public/shaders/availableShaders.conf")
+    val lines = availableShaders.mkString
+    availableShaders.close()
+
+    try {
+      val json = Json.parse(lines)
+      Ok(json)
+    } catch {
+      case e: Exception =>
+        InternalServerError("There was an error retrieving the available shaders.")
+    }
+  }
+
+  def getShaderByName = Action {
+    Ok
+  }
+
   def socket = WebSocket.using[String] {
     request =>
 
-      // Send a single 'Hello!' message and close
+    // Send a single 'Hello!' message and close
       val out = Enumerator.imperative[String]()
 
       // Just consume and ignore the input
