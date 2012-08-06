8d61d452648b9ae9530bdd01322bb55ab0635629
Stefan Reichel <dev@bomret.com>
2012-08-07 00:32:50 +0200

    Toyed around with the status output colors. Added documentation to FindNodes.js and others.

--- public/javascripts/domain/scene/findNodes.js
+++ public/javascripts/domain/scene/findNodes.js
@@ -2,12 +2,24 @@
  * User: Stefan Reichel
  * Date: 27.07.12
  * Time: 14:36
+ *
+ * This module contains methods to find nodes of a specific type or id in a given traversable scene.
  */
 
 define(["Utils", "TraverseScene"], function(Utils, TraverseScene) {
   "use strict";
 
   return {
+
+    /**
+     * This method searches the given scene for nodes of the given type. After the scene has been searched an array containing the found nodes is given to the callback. If an error occurs it will be given to the callback.
+     *
+     * In a traversable scene every node has a "type" property. It has nodes of type "group" with "children" properties which are arrays and contain other nodes.
+     *
+     * @param {Object} traversableScene The scene to be searched
+     * @param {String} nodeType The type of the nodes to be searched
+     * @param {Function} callback The callback that is executed either when an error is thrown or the search finishes.
+     */
     byType: function(traversableScene, nodeType, callback) {
       var foundNodes = [];
       try {
@@ -23,6 +35,15 @@ define(["Utils", "TraverseScene"], function(Utils, TraverseScene) {
       }
     },
 
+    /**
+     * This method searches the given scene for a node with the given id. After the scene has been searched the found node is given to the callback. If an error occurs it will be given to the callback.
+     *
+     * In a traversable scene every node has an "id" property. It has nodes of type "group" with "children" properties which are arrays and contain other nodes.
+     *
+     * @param {Object} traversableScene The scene to be searched
+     * @param {String} nodeId The id of the node to be found
+     * @param {Function} callback The callback that is executed either when an error is thrown or the search finishes.
+     */
     byId: function(traversableScene, nodeId, callback) {
       var foundNode = null;
       try {
